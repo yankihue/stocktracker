cd5573c87e02b51da0c3ac749f217d258be1d671
Yankı Sırtaş <yankisirtas@hotmail.com>
2023-08-09 17:21:48 +0300

    Add tasks

--- stocktracker/stocktracker/celery.py
+++ stocktracker/stocktracker/celery.py
@@ -29,13 +29,3 @@ app.autodiscover_tasks()
 @app.task(bind=True, ignore_result=True)
 def debug_task(self):
     print(f"Request: {self.request!r}")
-
-
-@shared_task
-def add(x, y):
-    return x + y
-
-
-@shared_task
-def mul(x, y):
-    return x * y
