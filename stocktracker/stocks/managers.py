0a26148c1cc968667ce58dd41d0c8653ae48fb7f
Yankı Sırtaş <yankisirtas@hotmail.com>
2023-08-09 18:42:38 +0300

    Add dynamic currencies to stock manager

--- stocktracker/stocks/managers.py
+++ stocktracker/stocks/managers.py
@@ -7,12 +7,12 @@ from django.db import models
 class StockManager(models.Manager):
     """A manager for the Stock model."""
 
-    def populate(self, request):
+    def populate(self, stock):
         """Populates the database with Stock objects."""
-        currency1 = "BTC"
+        currency = stock.ticker
         base_currency = "USD"
         url = "https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency={}&to_currency={}&apikey=WX0W0TEMMGW2CVT9".format(
-            currency1, base_currency
+            currency, base_currency
         )
 
         observation = requests.get(url=url).json()
@@ -20,13 +20,9 @@ class StockManager(models.Manager):
             observation["Realtime Currency Exchange Rate"]
         except KeyError:  # means we hit API limit
             return None
-        tickerName = observation["Realtime Currency Exchange Rate"][
-            "1. From_Currency Code"
-        ]
 
-        stock = self.get(ticker=tickerName)
         rate = observation["Realtime Currency Exchange Rate"]["5. Exchange Rate"]
         stock.price = float(rate)
         stock.save()
 
-        return stock
+        return self
