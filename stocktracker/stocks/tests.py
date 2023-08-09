bd057fea6d7d7df19b322122b076edc88ba9fe14
Yankı Sırtaş <yankisirtas@hotmail.com>
2023-08-09 15:20:00 +0300

    Create module

---
+++ stocktracker/stocks/tests.py
@@ -0,0 +1,10 @@
+# Create your tests here.
+from django.test import TestCase
+
+
+class DefaultTest(TestCase):
+    def setUp(self):
+        pass
+
+    def test_X(self):
+        self.assertEqual(3, 3)
